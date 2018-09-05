fffe34f4cc51ee54060c0277d6554981f80bdc2b
Katheryn Ford <AnEvilHerbivoreLP@gmail.com>
2018-09-05 10:14:40 -0500

    Can draw cards face-up and face-down using modal menu

--- room_service/static/scripts/deck.js
+++ room_service/static/scripts/deck.js
@@ -1,15 +1,47 @@
-let DJANGO_STATIC_URL = '{{ STATIC_URL }}'
-
 
 $.ajax('/card/').then(card_list => {
-    let unsorted_library = []
+    let library_list = []
+    let face_down_cards = []
     card_list.forEach(card => {
         for (i = 0; i < card.quantity; i++) {
-            unsorted_library.push({'name': card.name, 'image': card.image})
+            library_list.push({'name': card.name, 'image': card.image})
         }
     })
     
-    shuffle(unsorted_library)
+    shuffle(library_list)
+
+    $('#library').click(() => {
+        $('body').append(`
+            <div id='modal'>
+                <div id='modal-content'>
+                    <h3>Draw face-up or face-down?</h3>
+                    <input type='button' value="Face-up" class="face-up confirm">
+                    <input type='button' value="Face-down" class="face-down confirm">
+                    <input type='button' value="Cancel" class="cancel confirm">
+                </div>
+            </div>
+        `)
+
+        $('.confirm').click((e) => {
+            $('#modal').remove()
+            // $('#content').append()
+            if ($(e.target).hasClass('face-up')) {
+                $('#content').append(`
+                    <img src='/Users/katherynford/workspace/Sector86/Sector86/room_service/static/${library_list[0]["image"]}' alt='${library_list[0]["image"]}' />
+                `)
+                library_list.shift()
+            }
+            else if ($(e.target).hasClass('face-down')) {
+                $('#content').append(`
+                    <img src='/Users/katherynford/workspace/Sector86/Sector86/room_service/static/card_images/CardBack.png' alt='Credit' />
+                `)
+                console.log(library_list)
+                face_down_cards.push(library_list.shift())
+                console.log(library_list)
+                console.log(face_down_cards)
+            }
+        })
+    })
 })
 
 function shuffle (a) {
